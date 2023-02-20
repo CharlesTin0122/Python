0e3b0f99b830fed6e084cec26e85e4e0398aeeef
CharlesTin0122 <tianchao0533@gmail.com>
2023-02-20 10:02:51 +0800

    new file:   quadrupedRigging/Maya 2018 horse/Rig_horse_001.0003.mb 	new file:   quadrupedRigging/ctrl_creater.pyc 	modified:   quadrupedRigging/quadruped_Auto_Rig.py

--- quadrupedRigging/quadruped_Auto_Rig.py
+++ quadrupedRigging/quadruped_Auto_Rig.py
@@ -7,10 +7,10 @@
 '''
 
 '''Quadruped Auto Rig'''
-
-import pymel.core as pm
+import sys
+sys.path.append(r'G:\Code\Python\quadrupedRigging')
 from ctrl_creater import *
-
+import pymel.core as pm
 '''Placement locs '''
 
 def placementLoc(name,tx,ty,tz,colorId=0):
@@ -46,7 +46,7 @@ hindLocGrp.zeroTransformPivots()
 
 frontToe = placementLoc('L_frontToe_Loc',15,0,77,15)
 frontAnkle = placementLoc('L_frontAnkle_Loc',15,22,67,15)
-frontknee = placementLoc('L_frontKnee_Loc',15,53,66,15)
+frontknee = placementLoc('L_frontKnee_Loc',15,53,68,15)
 frontUpperKnee = placementLoc('L_frontUpperKnee_Loc',15,96,63,15)
 frontFemur = placementLoc('L_frontFemure_Loc',15,126,72,15)
 frontPelvis = placementLoc('L_frontPelvis_Loc',0,145,60,17)
@@ -248,45 +248,90 @@ def parentMatch(obj1,obj2):
 	pm.delete(nod)
 	pm.select(cl=1)
 
-###creare L hind IK###
+'''---------------------------------Create IK---------------------------------'''
+###create L hind IK###
 
 L_hindLegIk = pm.ikHandle(name='L_hindLeg_IK',sj=L_hindLeg_IkJntList[0],ee=L_hindLeg_IkJntList[-3],solver='ikRPsolver')
-
 L_hindIkCtrl = ctrlCreater('L_hindLeg_IK_Ctrl',ik_CurInfo,6)
 L_hindIkCtrl_Grp = pm.group(L_hindIkCtrl,n='L_hindLeg_IK_Ctrl_GRP')
-
-pointMatch(L_hindIkCtrl_Grp,L_hindLegIk[0])
+pointMatch(L_hindIkCtrl_Grp,L_hindLeg_IkJntList[-2])
 pm.parent(L_hindLegIk[0],L_hindIkCtrl)
 pm.aimConstraint(L_hindIkCtrl,L_hindLeg_IkJntList[0],n='l_femur_aim_towards_footCtrl',mo=True,wu=[0,0,0])
+L_hindLegToeIk = pm.ikHandle(n='L_hindLegToe_IK',sol='ikRPsolver',sj=L_hindLeg_IkJntList[-3],ee=L_hindLeg_IkJntList[-2])
+pm.parent(L_hindLegToeIk[0],L_hindIkCtrl)
+#create L hind IK PoleVector
+L_hindIkPoleCtrl = ctrlCreater('L_hind_Ik_Pole_Ctrl',pole_CurInfo,6)
+L_hindIkPoleCtrlGrp = pm.group(L_hindIkPoleCtrl,n='L_hind_Ik_Pole_Ctrl_GRP')
+pointMatch(L_hindIkPoleCtrlGrp,L_hindLeg_IkJntList[2])
+L_hindIkPoleCtrlGrp.tz.set(L_hindIkPoleCtrlGrp.tz.get()*2)
+pm.poleVectorConstraint(L_hindIkPoleCtrl,L_hindLegIk[0])
+L_hindLegIk[0].twist.set(180)
 
 ###creare R hind IK###
 
 R_hindLegIk = pm.ikHandle(name='R_hindLeg_IK',sj=R_hindLeg_IkJntList[0],ee=R_hindLeg_IkJntList[-3],solver='ikRPsolver')
-
 R_hindIkCtrl = ctrlCreater('R_hindLeg_IK_Ctrl',ik_CurInfo,13)
 R_hindIkCtrR_Grp = pm.group(R_hindIkCtrl,n='R_hindLeg_IK_Ctrl_GRP')
-
-pointMatch(R_hindIkCtrR_Grp,R_hindLegIk[0])
+pointMatch(R_hindIkCtrR_Grp,R_hindLeg_IkJntList[-2])
 pm.parent(R_hindLegIk[0],R_hindIkCtrl)
 pm.aimConstraint(R_hindIkCtrl,R_hindLeg_IkJntList[0],n='R_femur_aim_towards_footCtrl',mo=True,wu=[0,0,0])
+R_hindLegToeIk = pm.ikHandle(n='R_hindLegToe_IK',sol='ikRPsolver',sj=R_hindLeg_IkJntList[-3],ee=R_hindLeg_IkJntList[-2])
+pm.parent(R_hindLegToeIk[0],R_hindIkCtrl)
+
+#create R hind IK PoleVector
+R_hindIkPoleCtrl = ctrlCreater('R_hind_Ik_Pole_Ctrl',pole_CurInfo,13)
+R_hindIkPoleCtrlGrp = pm.group(R_hindIkPoleCtrl,n='R_hind_Ik_Pole_Ctrl_GRP')
+pointMatch(R_hindIkPoleCtrlGrp,R_hindLeg_IkJntList[2])
+R_hindIkPoleCtrlGrp.tz.set(R_hindIkPoleCtrlGrp.tz.get()*2)
+pm.poleVectorConstraint(R_hindIkPoleCtrl,R_hindLegIk[0])
+R_hindLegIk[0].twist.set(180)
 
 ###creare L front IK###
 
 L_frontLegIk = pm.ikHandle(name='L_frontLeg_IK',sj=L_frontLeg_IkJntList[0],ee=L_frontLeg_IkJntList[-3],solver='ikRPsolver')
-
 L_frontIkCtrl = ctrlCreater('L_frontLeg_IK_Ctrl',ik_CurInfo,6)
 L_frontIkCtrl_Grp = pm.group(L_frontIkCtrl,n='L_frontLeg_IK_Ctrl_GRP')
-
-pointMatch(L_frontIkCtrl_Grp,L_frontLegIk[0])
+pointMatch(L_frontIkCtrl_Grp,L_frontLeg_IkJntList[-2])
 pm.parent(L_frontLegIk[0],L_frontIkCtrl)
+L_frontLegToeIk = pm.ikHandle(n='L_frontLegToe_IK',sol='ikRPsolver',sj=L_frontLeg_IkJntList[-3],ee=L_frontLeg_IkJntList[-2])
+pm.parent(L_frontLegToeIk[0],L_frontIkCtrl)
+
+L_frontLegRoCtrl = ctrlCreater('L_front_Leg_Rotete_Ctrl',fk_CurInfo,6)
+L_frontLegRoCtrlGrp = pm.group(L_frontLegRoCtrl,n='L_front_Leg_Rotete_Ctrl_GRP')
+pointMatch(L_frontLegRoCtrlGrp,L_frontLeg_IkJntList[0])
+
+#create L front IK PoleVector
+L_frontIkPoleCtrl = ctrlCreater('L_front_Ik_Pole_Ctrl',pole_CurInfo,6)
+L_frontIkPoleCtrlGrp = pm.group(L_frontIkPoleCtrl,n='L_front_Ik_Pole_Ctrl_GRP')
+pointMatch(L_frontIkPoleCtrlGrp,L_frontLeg_IkJntList[2])
+L_frontIkPoleCtrlGrp.tz.set(0)
+pm.poleVectorConstraint(L_frontIkPoleCtrl,L_frontLegIk[0])
 
 
 ###creare R front IK###
 
 R_frontLegIk = pm.ikHandle(name='R_frontLeg_IK',sj=R_frontLeg_IkJntList[0],ee=R_frontLeg_IkJntList[-3],solver='ikRPsolver')
-
 R_frontIkCtrl = ctrlCreater('R_frontLeg_IK_Ctrl',ik_CurInfo,13)
 R_frontIkCtrR_Grp = pm.group(R_frontIkCtrl,n='R_frontLeg_IK_Ctrl_GRP')
-
-pointMatch(R_frontIkCtrR_Grp,R_frontLegIk[0])
+pointMatch(R_frontIkCtrR_Grp,R_frontLeg_IkJntList[-2])
 pm.parent(R_frontLegIk[0],R_frontIkCtrl)
+R_frontLegToeIk = pm.ikHandle(n='R_frontLegToe_IK',sol='ikRPsolver',sj=R_frontLeg_IkJntList[-3],ee=R_frontLeg_IkJntList[-2])
+pm.parent(R_frontLegToeIk[0],R_frontIkCtrl)
+
+R_frontLegRoCtrl = ctrlCreater('R_front_Leg_Rotete_Ctrl',fk_CurInfo,6)
+R_frontLegRoCtrlGrp = pm.group(R_frontLegRoCtrl,n='R_front_Leg_Rotete_Ctrl_GRP')
+pointMatch(R_frontLegRoCtrlGrp,R_frontLeg_IkJntList[0])
+
+#create R front IK PoleVector
+R_frontIkPoleCtrl = ctrlCreater('R_front_Ik_Pole_Ctrl',pole_CurInfo,13)
+R_frontIkPoleCtrlGrp = pm.group(R_frontIkPoleCtrl,n='R_front_Ik_Pole_Ctrl_GRP')
+pointMatch(R_frontIkPoleCtrlGrp,R_frontLeg_IkJntList[2])
+R_frontIkPoleCtrlGrp.tz.set(0)
+pm.poleVectorConstraint(R_frontIkPoleCtrl,R_frontLegIk[0])
+
+#Group IK Items
+poleVectorGrp = pm.group(L_frontIkPoleCtrlGrp,R_frontIkPoleCtrlGrp,L_hindIkPoleCtrlGrp,R_hindIkPoleCtrlGrp,n='pole_Vector_GRP')
+IkCtrlGrp = pm.group(poleVectorGrp,
+					L_hindIkCtrl_Grp,R_hindIkCtrR_Grp,L_frontIkCtrl_Grp,R_frontIkCtrR_Grp,
+					L_frontLegRoCtrlGrp,R_frontLegRoCtrlGrp,n='IK_Ctrl_Grp')
+IkCtrlGrp.zeroTransformPivots()
