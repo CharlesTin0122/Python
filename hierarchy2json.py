355187e727ab46bea6a58a217604296fd9383296
CharlesTin0122 <tianchao0533@gmail.com>
2023-03-29 16:28:08 +0800

    modified:   hierarchy2json.py 	modified:   test.py

--- hierarchy2json.py
+++ hierarchy2json.py
@@ -3,26 +3,33 @@ import pymel.core.nodetypes as nt
 import json
 
 def hierarchy2json(parents,dump2json=True,tree=None,init=True,):
-	"""选择一个物体，获取物体之下的子物体 转为json存储
+	"""
+	该函数的作用是将指定的父节点列表转换为 JSON 格式的层级结构。具体实现方法为：
+	遍历每个父节点，初始化一个子字典，然后递归处理其子节点，并将子节点添加到子字典中。
+	如果遍历完了所有的父节点，则返回处理结果。
+	其中，如果指定了 dump2json 参数为 True,则将结果转换为 JSON 格式并返回；否则返回字典格式的结果。
 
 	Args:
-		parents (_type_): _description_
-		dump2json (bool, optional): _description_. Defaults to True.
-		tree (_type_, optional): _description_. Defaults to None.
-		init (bool, optional): _description_. Defaults to True.
+		parents (list): 是指定的父节点列表
+		dump2json (bool, optional): 是否将结果转换为 JSON 格式并返回
+		tree (dict, optional): 指定的目标字典
+		init (bool, optional): 是否为根节点
 
 	Returns:
-		_type_: _description_
+		dict or json: 字典
 	"""
+	# 如果 parents 不是列表类型，则转换为列表类型
 	parents = parents if isinstance(parents,list) else [parents]
+	# 如果 tree 不是字典类型，则初始化一个字典
 	_tree = {str(p):{} if isinstance(p,nt.Transform) else p.type() for p in parents} if not isinstance(tree,dict) else tree
-
+	 # 遍历每个父节点
 	for parent in parents:
-		tree = _tree[str(parent)] if init else _tree
-		for child in parent.getChildren():
-			tree[str(child)] = {} if isinstance(child,nt.Transform) else child.type()
-			hierarchy2json(child,tree=tree[str(child)],init=False)
-
+		tree = _tree[str(parent)] if init else _tree # 如果不是根节点，则使用指定的 tree 字典
+		# 遍历每个子节点
+		for child in parent.getChildren(): 
+			tree[str(child)] = {} if isinstance(child,nt.Transform) else child.type() # 初始化子节点的字典
+			hierarchy2json(child,tree=tree[str(child)],init=False)# 递归处理子节点
+	# 如果是根节点，则返回处理结果（可以选择将结果转换为 JSON 格式）
 	if init:
 		return json.dumps(_tree) if dump2json else _tree
 
@@ -31,15 +38,17 @@ def hierarchy2json(parents,dump2json=True,tree=None,init=True,):
 
 print((hierarchy2json(pm.selected())))
 
-#方法2
+
+'''-----------------------------------------------------------------------方法2-------------------------------------------------------------------------'''
+
 import maya.cmds as mc
 
 def hierarchyTree(parent, tree):
-    children = mc.listRelatives(parent, c=True, type='transform')
-    if children:
-        tree[parent] = (children, {})
-        for child in children:
-            hierarchyTree(child, tree[parent][1])
+	children = mc.listRelatives(parent, c=True, type='transform')
+	if children:
+		tree[parent] = (children, {})
+		for child in children:
+			hierarchyTree(child, tree[parent][1])
 
 top_node = mc.ls(sl=True)[0]
 hierarchy_tree = {}
@@ -47,9 +56,9 @@ hierarchyTree(top_node, hierarchy_tree)
 
 
 def reparent(tree):
-    for parent, data in tree.iteritems():
-        children, child_tree = data
-        mc.parent(children, parent)
-        reparent(child_tree)
+	for parent, data in tree.iteritems():
+		children, child_tree = data
+		mc.parent(children, parent)
+		reparent(child_tree)
 
 reparent(hierarchy_tree)
--- test.py
+++ test.py
@@ -1,56 +1,27 @@
 import pymel.core as pm
-import pymel.core.nodetypes as nt
 import json
 
-def hierarchy2json(parents,dump2json=True,tree=None,init=True,):
-	"""选择一个物体，获取物体之下的子物体 转为json存储
+def get_joint_hierarchy(root_joint):
+    """
+    获取骨骼层级信息
+    :param root_joint: 根骨骼
+    :return: 层级列表
+    """
+    hierarchy = []
+    joint_list = [root_joint]
+    while joint_list:
+        current_joint = joint_list.pop(0)
+        hierarchy.append(current_joint.name())
+        children = current_joint.getChildren(ad=True, type='joint')
+        joint_list.extend(children)
+    return hierarchy
 
-	Args:
-		parents (_type_): _description_
-		dump2json (bool, optional): _description_. Defaults to True.
-		tree (_type_, optional): _description_. Defaults to None.
-		init (bool, optional): 是否写入json. Defaults to True.
+# 获取场景中的根骨骼
+root_joint = pm.ls(type='joint', l=True)[0]
 
-	Returns:
-		_type_: _description_
-	"""
-	parents = parents if isinstance(parents,list) else [parents]
-	_tree = {str(p):{} if isinstance(p,nt.Transform) else p.type() for p in parents} if not isinstance(tree,dict) else tree
+# 获取骨骼层级信息
+joint_hierarchy = get_joint_hierarchy(root_joint)
 
-	for parent in parents:
-		tree = _tree[str(parent)] if init else _tree
-		for child in parent.getChildren():
-			tree[str(child)] = {} if isinstance(child,nt.Transform) else child.type()
-			hierarchy2json(child,tree=tree[str(child)],init=False)
-
-	if init:
-		return json.dumps(_tree) if dump2json else _tree
-
-# for sel in pm.selected():
-#     print(json.dumps(hierarchy2json(sel)))
-
-print((hierarchy2json(pm.selected())))
-
-
-
-import maya.cmds as mc
-
-def hierarchyTree(parent, tree):
-    children = mc.listRelatives(parent, c=True, type='transform')
-    if children:
-        tree[parent] = (children, {})
-        for child in children:
-            hierarchyTree(child, tree[parent][1])
-
-top_node = mc.ls(sl=True)[0]
-hierarchy_tree = {}
-hierarchyTree(top_node, hierarchy_tree)
-
-
-def reparent(tree):
-    for parent, data in tree.iteritems():
-        children, child_tree = data
-        mc.parent(children, parent)
-        reparent(child_tree)
-
-reparent(hierarchy_tree)
+# 将骨骼层级信息写入JSON文件
+with open('joint_hierarchy.json', 'w') as f:
+    json.dump(joint_hierarchy, f)
