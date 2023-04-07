de637573754050fd1c4c145692ef7f0f0fc463fd
CharlesTin0122 <tianchao0533@gmail.com>
2023-04-07 16:14:11 +0800

    modified:   .idea/Python.iml 	modified:   Examples/batch_constrain_joint.py 	modified:   Examples/select_joint_hierarchy.py

--- Examples/select_joint_hierarchy.py
+++ Examples/select_joint_hierarchy.py
@@ -1,5 +1,10 @@
 import pymel.core as pm
 
-selection = pm.selected() #根骨骼放入变量
-joints = pm.ls(selection[0], dag=True, type="joint") #列出骨骼链的所有骨骼，注意参数dag
-pm.select(joints)#选择所有骨骼
+selection = pm.selected()  # 根骨骼放入变量
+joints = pm.ls(selection[0], dag=True, type="joint")  # 列出骨骼链的所有骨骼，注意参数dag
+pm.select(joints)  # 选择所有骨骼
+
+
+selection = pm.selected()  # 根骨骼放入变量
+joints = pm.ls(selection[0], dag=True, type="constraint")  # 列出骨骼链的所有约束节点，注意参数dag
+pm.select(joints)  # 选择所有约束节点
