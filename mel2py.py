f42e9274af70d3ad49a67f057a469248c3579125
CharlesTin0122 <tianchao0533@gmail.com>
2023-04-13 13:52:14 +0800

    modified:   Examples/batch_constrain_joint.py 	new file:   Learnning/ui.py 	modified:   mel2py.py 	modified:   test001.py

--- mel2py.py
+++ mel2py.py
@@ -1,8 +1,36 @@
 # -*- coding: utf-8 -*-
-# @FileName :  mel2py.py
-# @Author   : TianChao
-# @Email    : tianchao0533@gamil.com
-# @Time     :  2023/4/11 15:46
-# @Software : PyCharm
-# Description:
+"""
+@FileName    :   mel2py.py
+@DateTime    :   2023/04/13 10:57:57
+@Author  :   Tian Chao
+@Contact :   tianchao0533@163.com
+"""
 
+import pymel.core as pm
+import pymel.tools.mel2py as mel2py
+
+
+def mel2pymel(*args):
+    melCmd = pm.scrollField("melcode", q=1, tx=1)
+    pyCmd = mel2py.mel2pyStr(melCmd, pymelNamespace="pm")
+    pyFixed = pyCmd.replace("pymel.all", "pymel.core")
+    pm.scrollField("pymelcode", e=1, tx=pyFixed)
+
+
+def mel2py_ui():
+    try:
+        pm.deleteUI("melpy")
+    except Exception as e:
+        print(e)
+
+    with pm.window("melpy", title="mel2py", h=150, w=240):
+        with pm.columnLayout(adj=1):
+            with pm.frameLayout(label="mel"):
+                pm.scrollField("melcode", h=100, w=200)
+            with pm.frameLayout(label="pymel"):
+                pm.scrollField("pymelcode", h=100, w=200)
+        with pm.columnLayout(adj=1):
+            pm.button(label="Tanslate Mel To Pymel", w=80, h=30, c=mel2pymel)
+
+
+mel2py_ui()
--- test001.py
+++ test001.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/4/3 13:32
-# @Author  : TianChao
-# @File    : test001.py
-# @Email: tianchao0533@gamil.com
-# @Software: PyCharm
+"""
+@FileName    :   test001.py
+@DateTime    :   2023/04/12 18:07:37
+@Author  :   Tian Chao
+@Contact :   tianchao0533@163.com
+"""
 
+import pymel.core as pm
+
+val = []
+
+
+def copy(*args):
+    global val
+    sel_obj = pm.selected()
+    obj_attr = pm.listAnimatable(sel_obj)
+    val = [pm.getAttr(attr) for attr in obj_attr]
+
+
+def paste(*args):
+    sel_obj = pm.selected()
+    obj_attr = pm.listAnimatable(sel_obj)
+    for i in range(len(obj_attr)):
+        if not obj_attr[i].find("translateX") == -1:
+            pm.setAttr(obj_attr[i], val[i] * -1)
+        else:
+            pm.setAttr(obj_attr[i], val[i])
+
+
+def mainUI():
+    try:
+        pm.deleteUI("pos_tool")
+    except Exception as e:
+        print(e)
+
+    pm.window("pos_tool", title="pos_tool", w=240, h=300)
+    pm.columnLayout(rowSpacing=5, adj=True)
+    pm.button(label="copy", c=copy, w=20, h=80)
+    pm.button(label="past", c=paste, w=20, h=80)
+    pm.showWindow("pos_tool")
+
+
+mainUI()
