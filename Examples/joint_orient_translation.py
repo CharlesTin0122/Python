a26537caf2d49a1b617096ab3ca98880a2519835
CharlesTin0122 <tianchao0533@gmail.com>
2023-04-20 15:49:47 +0800

    新增案例，和修改之前文件

---
+++ Examples/joint_orient_translation.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+@FileName    :   joint_orient_translation.py
+@DateTime    :   2023/04/20 11:12:22
+@Author  :   Tian Chao
+@Contact :   tianchao0533@163.com
+"""
+
+import json
+import pymel.core as pm
+# 获取json文件路径
+jo_json_path = r'D:\Backup\Documents\maya\scripts\joint_orient.json'
+
+
+def joint_orient_to_json(dady_joint: str, rite_json_path: str) -> dict:
+	"""
+	将父骨骼之下的所有骨骼的joint orient值写入json文件
+	Args:
+		dady_joint: 父骨骼
+		rite_json_path: json文件路径
+
+	Returns:骨骼轴向字典
+
+	"""
+	selection = pm.PyNode(dady_joint)  # 父骨骼放入变量
+
+	joints = pm.ls(selection[0], dag=True, type="joint")  # 列出骨骼链的所有骨骼
+
+	jnt_jo_attr = ['{}.jointOrient'.format(jnt) for jnt in joints]  # 获取骨骼jointOrient属性列表
+	jnt_jo_val = [pm.getAttr(val) for val in jnt_jo_attr]  # 获取获取骨骼jointOrient属性列表属性值向量列表
+	jnt_jo_val_list = [list(vec) for vec in jnt_jo_val]  # 获取获取骨骼jointOrient属性列表属性值列表
+	data = dict(zip(jnt_jo_attr, jnt_jo_val_list))  # 将属性和属性值打包成字典
+
+	jsondata = json.dumps(data)  # 将字典转化成json格式
+	with open(rite_json_path, 'w') as f:  # 打开json文件
+		f.write(jsondata)  # 写入字典数据
+	return data  # 返回字典
+
+
+def json_to_joint_orient(mesh_list: list, read_json_path: str) -> dict:
+	"""
+
+	Args:
+		mesh_list: 要修改蒙皮的模型列表
+		read_json_path: json文件路径
+
+	Returns:骨骼轴向字典
+
+	"""
+	skin_clusters = [pm.listHistory(mesh, type='skinCluster') for mesh in mesh_list]  # 获取所有模型的蒙皮节点
+	# 将所有蒙皮节点打开骨骼移动模式
+	for cluster in skin_clusters:
+		pm.skinCluster(cluster, edit=True, moveJointsMode=True)
+	# 读取并转码json文件数据
+	with open(read_json_path, 'r') as f:
+		data = f.read()
+	dict_data = json.JSONDecoder().decode(data)
+	# del dict_data['facebase.jointOrient']
+
+	# 设置骨骼属性
+	for k, v in dict_data.items():
+		pm.setAttr(k, v)
+	# 将所有蒙皮节点关闭骨骼移动模式
+	for cluster in skin_clusters:
+		pm.skinCluster(cluster, edit=True, moveJointsMode=False)
+
+	jnt = pm.skinCluster(skin_clusters[0], query=True, inf=True)[1]  # 通过蒙皮节点获取一根骨骼
+	root_joint = jnt.getAllParents()[-1]  # 获取该骨骼的根骨骼
+	jnt_list = pm.ls(root_joint, dag=True, type="joint")  # 获取该根骨骼的整个骨骼链
+	pm.select(jnt_list)  # 选中骨骼链中的所有骨骼
+	dag_pose = pm.dagPose(bindPose=True, q=True)  # 获取所有骨骼的绑定姿态
+	pm.delete(dag_pose)  # 删除所有绑定姿态
+	pm.dagPose(bindPose=True, save=True)  # 创建新绑定姿态
+	return dict_data  # 返回属性信息
--- Learnning/Json.py
+++ Learnning/Json.py
@@ -1,4 +1,4 @@
-#案例1
+"""-----------------------------------------------------案例1--------------------------------------------------------"""
 
 import json
 '''
@@ -6,43 +6,42 @@ import json
 '''
 data = {
     "abc": 123,
-    "Path": "D:\Work_MobilGame\Test",
+    "Path": r"D:\Work_MobilGame\Test",
     "123": 1.0
 }
 
-#写入
-with open("testttttt.json","w") as f:
+#  写入
+with open("testttttt.json", "w") as f:
     json.dump(data, f, indent=4)
 
 
-#读取
+#  读取
 with open("test.json", "r") as f:
     data = json.load(f)
 print(data)
 
-#案例2
+"""--------------------------------------------------------案例2-----------------------------------------------------"""
 
-import maya.cmds as mc
-#创建一一对应列表
-attrList = ['translateX','translateY','translateZ','rotateX','rotateY','rotateZ','scaleX','scaleY','scaleZ']
-attrVal = [1.046,1.712,3.438,-14.464,15.652,50.186,1,1,1]
-#将两个列表压缩成一一对应的元组列表
-zipList = zip(attrList,attrVal)
-#将一一对应的元组列表生成字典
+#  创建一一对应列表
+attrList = ['translateX', 'translateY', 'translateZ', 'rotateX', 'rotateY', 'rotateZ', 'scaleX', 'scaleY', 'scaleZ']
+attrVal = [1.046, 1.712, 3.438, -14.464, 15.652, 50.186, 1, 1, 1]
+#  将两个列表压缩成一一对应的元组列表
+zipList = zip(attrList, attrVal)
+#  将一一对应的元组列表生成字典
 data = dict(zipList)
 
-import json
-#设置保存路径和写入的变量
+
+#  设置保存路径和写入的变量
 path = r'C:\Users\tianc\Documents\maya\2020\prefs\scripts\pSphere1.json'
 jsonData = json.dumps(data)
-#写入
-with open(path,'w') as f:
-	f.write(jsonData)
-#读取
-with open(path,'r') as f:
-	sourceData = f.read()
-#编码为maya可用
+# 写入
+with open(path, 'w') as f:
+    f.write(jsonData)
+#  读取
+with open(path, 'r') as f:
+    sourceData = f.read()
+# 编码为maya可用
 targetData = json.JSONDecoder().decode(sourceData)
-#字典循环使用方法
-for key,value in targetData.items():
-	print(key,value)
+# 字典循环使用方法
+for key, value in targetData.items():
+    print(key, value)
