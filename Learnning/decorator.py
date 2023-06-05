e8b976d31ff6bd30a2f0a491e8699a123eead47a
CharlesTin0122 <tianchao0533@gmail.com>
2023-06-05 14:13:34 +0800

    modified:   Learnning/decorator.py 	modified:   Learnning/maya_api/maya_api_learning.py

---
+++ Learnning/decorator.py
@@ -0,0 +1,30 @@
+# 类的单例模式
+class Printer(object):
+    tasks = []
+    instance = None  # 存放第一个实例对象
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def add_tasks(self, job):
+        self.tasks.append(job)
+        print("{} 添加任务 {}到打印机，总任务数{}".format(self.name, job, len(self.tasks)))
+
+    # 只有第一次实例化的时候正常进行，后面每次实例化，并不真的创建一个新实例
+    def __new__(cls, *args, **kwargs):
+        # 正常进行实例化，并把实例化后的对象 存在cls.instance里面
+        if cls.instance is None:
+            obj = object.__new__(cls)  # 实例化过程
+            print(obj)
+            cls.instance = obj  # 把实例化好的对象存下来
+        # 以后每次实例化，直接返回第一次存的实例对象，在上一次实例对象基础上，再执行__init__
+        return cls.instance
+
+
+p1 = Printer("word app")
+p2 = Printer("ppt app")
+p3 = Printer("excel app")
+p1.add_tasks("word file")
+p2.add_tasks("ppt file")
+p3.add_tasks("excel file")
+print(p1.name, p2.name, p3.name)
--- Learnning/maya_api/maya_api_learning.py
+++ Learnning/maya_api/maya_api_learning.py
@@ -47,7 +47,7 @@ def selected_by_name(name_list: list):
     OpenMaya.MGlobal.setActiveSelectionList(sel)
 
 
-selected_by_name(['pCube2', 'pSphere1'])
+selected_by_name(["pCube2", "pSphere1"])
 
 
 def is_all():
@@ -95,7 +95,9 @@ def find_hierarchy():
     sel_dag_path = OpenMaya.MDagPath()  # 创建MDagPath实例
     selected.getDagPath(0, sel_dag_path)  # 将选择列表对象的第0个装入MDagPath实例
 
-    sel_dag_path_fn = OpenMaya.MFnDagNode(sel_dag_path)  # 创建DagNode函数集，将sel_dag_path装入函数集
+    sel_dag_path_fn = OpenMaya.MFnDagNode(
+        sel_dag_path
+    )  # 创建DagNode函数集，将sel_dag_path装入函数集
     parent_obj = sel_dag_path_fn.parent(0)  # 调用函数集的parent方法获取父物体
     parent_obj_fn = OpenMaya.MFnDagNode(parent_obj)  # 将父对象装入函数集
     print(parent_obj_fn.name())  # 调用函数集name方法获得名称
@@ -108,7 +110,7 @@ def find_hierarchy():
         child_obj_list.append(child_obj)
 
     sel_dag_path_fn.duplicate()  # 复制对象
-    sel_dag_path_fn.setName('aaa')  # 重命名对象
+    sel_dag_path_fn.setName("aaa")  # 重命名对象
     sel_dag_path_fn.removeChild(child_obj_list[-1])  # 移除子物体
     parent_obj_fn.addChild(child_obj_list[-1])  # 添加子物体
 
@@ -124,7 +126,9 @@ def create_node():
 
     """
     parent_dag_path_fn = OpenMaya.MFnDagNode()  # 实例化MFnDagNode函数集
-    parent_obj = parent_dag_path_fn.create("transform", "parent")  # 使用MFnDagNode类的方法创建节点
+    parent_obj = parent_dag_path_fn.create(
+        "transform", "parent"
+    )  # 使用MFnDagNode类的方法创建节点
 
     child_dag_path_fn = OpenMaya.MFnDagNode()  # 实例化MFnDagNode函数集
     # 使用MFnDagNode类的方法创建节点，三个参数，节点类型，节点名称，节点父对象
@@ -145,7 +149,9 @@ def list_attr():
     Returns:None
 
     """
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
     OpenMaya.MGlobal.getActiveSelectionList(selected)  # 获取当前选择对象装入选择列表
     obj = OpenMaya.MObject()  # 实例化MObject对象
     selected.getDependNode(0, obj)  # 将选择列表第一项获取依赖节点并装入MObject对象
@@ -166,7 +172,9 @@ def get_set_attr():
     Returns:None
 
     """
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
     OpenMaya.MGlobal.getActiveSelectionList(selected)  # 获取当前选择对象装入选择列表
     obj = OpenMaya.MObject()  # 实例化MObject对象
     selected.getDependNode(0, obj)  # 将选择列表第一项获取依赖节点并装入MObject对象
@@ -213,7 +221,9 @@ def add_attr():
     setConnectable()：设置属性是否可连接。
     setWritable()：设置属性是否可写。
     """
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
     OpenMaya.MGlobal.getActiveSelectionList(selected)  # 获取当前选择对象装入选择列表
     obj = OpenMaya.MObject()  # 实例化MObject对象
     selected.getDependNode(0, obj)  # 将选择列表第一项获取依赖节点并装入MObject对象
@@ -221,7 +231,9 @@ def add_attr():
     #  OpenMaya.MFnNumericAttribute为 OpenMaya.MFnAttribute的子类之一，意为数字类属性
     fn_num_attr = OpenMaya.MFnNumericAttribute()  # 创建一个数字类属性实例
     # 属性的创建，第一个参数为属性长名称，第二个参数为属性短名称，第三个属性为数据类型为双精度浮点型，第四个属性为默认值
-    attr_obj = fn_num_attr.create("myNewAttr", "MNA", OpenMaya.MFnNumericData.kDouble, 1.0)
+    attr_obj = fn_num_attr.create(
+        "myNewAttr", "MNA", OpenMaya.MFnNumericData.kDouble, 1.0
+    )
     fn_num_attr.setKeyable(True)  # 属性设置为可尅帧
     obj_dep_nod_fn.addAttribute(attr_obj)  # 添加属性到attr_obj
 
@@ -235,7 +247,9 @@ def del_attr():
     Returns:None
 
     """
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
     OpenMaya.MGlobal.getActiveSelectionList(selected)  # 获取当前选择对象装入选择列表
     obj = OpenMaya.MObject()  # 实例化MObject对象
     selected.getDependNode(0, obj)  # 将选择列表第一项获取依赖节点并装入MObject对象
@@ -256,19 +270,29 @@ def double_array_attr():
     Returns:None
 
     """
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected
     selected.add("skinCluster1")  # 将名为"skinCluster1"的节点添加到选择列表selected中
-    skin_cluster = OpenMaya.MObject()  # 实例化OpenMaya.MObject()创建一个MObject对象skin_cluster，用于存储皮肤集群节点。
+    skin_cluster = (
+        OpenMaya.MObject()
+    )  # 实例化OpenMaya.MObject()创建一个MObject对象skin_cluster，用于存储皮肤集群节点。
     selected.getDependNode(0, skin_cluster)  # 将选择列表中的第一个节点作为依赖节点获取并存储到skin_cluster中
 
-    skin_fn_node = OpenMaya.MFnDependencyNode(skin_cluster)  # 创建一个依赖节点函数集对象skin_fn_node，用于操作依赖节点
+    skin_fn_node = OpenMaya.MFnDependencyNode(
+        skin_cluster
+    )  # 创建一个依赖节点函数集对象skin_fn_node，用于操作依赖节点
     skin_plug = skin_fn_node.findPlug("ptw")  # 找到名为"ptw"的属性接口skin_plug
-    assert isinstance(skin_plug, OpenMaya.MPlug)  # 断言属性接口变量skin_plug是OpenMaya.MPlug对象，以便在编码时获得自动补全功能
+    assert isinstance(
+        skin_plug, OpenMaya.MPlug
+    )  # 断言属性接口变量skin_plug是OpenMaya.MPlug对象，以便在编码时获得自动补全功能
     array_obj = skin_plug.asMObject()  # 将属性接口转换为MObject对象array_obj
     # 转化MObject对象为双精度数组数据函数集对象，用于操作双精度数组数据
     fn_doubel_array_data = OpenMaya.MFnDoubleArrayData(array_obj)
     weights = fn_doubel_array_data.array()  # 获取双精度数组属性的值，存储在weights变量中。
-    assert isinstance(weights, OpenMaya.MDoubleArray)  # 断言weights变量是OpenMaya.MDoubleArray对象，以便获得自动补全功能
+    assert isinstance(
+        weights, OpenMaya.MDoubleArray
+    )  # 断言weights变量是OpenMaya.MDoubleArray对象，以便获得自动补全功能
     # 遍历双精度数组属性的每个元素，并打印其值。然后使用weights.set(0.5, i)将每个元素的值设置为0.5。
     for i in range(weights.length()):
         print(weights[i])
@@ -288,29 +312,45 @@ def matrix_array():
 
     """
     """获取骨骼位置矩阵"""
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected。
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected。
     selected.add("joint2")  # 将名为"joint2"的节点添加到选择列表selected中。
     joint_obj = OpenMaya.MObject()  # 创建一个MObject对象joint_obj，用于存储选择列表中的第一个骨骼节点。
     selected.getDependNode(0, joint_obj)  # 将选择列表中的第一个节点获取并存储到joint_obj中。
-    joint_fn_node = OpenMaya.MFnDependencyNode(joint_obj)  # 创建一个依赖节点函数集对象joint_fn_node，用于操作骨骼节点。
+    joint_fn_node = OpenMaya.MFnDependencyNode(
+        joint_obj
+    )  # 创建一个依赖节点函数集对象joint_fn_node，用于操作骨骼节点。
     matrix_plug = joint_fn_node.findPlug("matrix")  # 找到名为"matrix"的属性接口matrix_plug。
     assert isinstance(matrix_plug, OpenMaya.MPlug)  # 断言属性接口变量
     matrix_obj = matrix_plug.asMObject()  # 将属性接口转换为MObject对象matrix_obj。
-    fn_matrix_data = OpenMaya.MFnMatrixData(matrix_obj)  # 创建一个矩阵数据函数集对象fn_matrix_data，用于操作矩阵数据。
+    fn_matrix_data = OpenMaya.MFnMatrixData(
+        matrix_obj
+    )  # 创建一个矩阵数据函数集对象fn_matrix_data，用于操作矩阵数据。
     matrix = fn_matrix_data.matrix()  # 获取矩阵属性的值，存储在matrix变量中。
     """获取蒙皮节点bindPreMatrix绑定前矩阵，意为绑定姿态矩阵"""
-    selected = OpenMaya.MSelectionList()  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected。
+    selected = (
+        OpenMaya.MSelectionList()
+    )  # 通过实例化OpenMaya.MSelectionList()创建一个选择列表对象selected。
     selected.add("skinCluster1")  # 将名为"skinCluster1"的节点添加到选择列表中。
     skin_obj = OpenMaya.MObject()  # 创建一个MObject对象skin_obj，用于存储选择列表中的第一个蒙皮节点。
     selected.getDependNode(0, skin_obj)  # 将选择列表中的第一个节点获取依赖节点并存储到skin_obj中。
-    skin_fn_node = OpenMaya.MFnDependencyNode(skin_obj)  # 创建一个依赖节点函数集对象skin_fn_node，用于操作蒙皮节点。
-    skin_plug = skin_fn_node.findPlug("bindPreMatrix")  # 找到名为"bindPreMatrix"的属性接口skin_plug。
+    skin_fn_node = OpenMaya.MFnDependencyNode(
+        skin_obj
+    )  # 创建一个依赖节点函数集对象skin_fn_node，用于操作蒙皮节点。
+    skin_plug = skin_fn_node.findPlug(
+        "bindPreMatrix"
+    )  # 找到名为"bindPreMatrix"的属性接口skin_plug。
     assert isinstance(skin_plug, OpenMaya.MPlug)  # 断言属性接口变量
-    plug_0 = skin_plug.elementByLogicalIndex(1)  # 获取属性接口的第二个子接口，即数组的第二个元素，对应“joint2”的蒙皮。
+    plug_0 = skin_plug.elementByLogicalIndex(
+        1
+    )  # 获取属性接口的第二个子接口，即数组的第二个元素，对应“joint2”的蒙皮。
     """设置绑定姿态矩阵为骨骼逆矩阵"""
     fn_matrix_data = OpenMaya.MFnMatrixData()  # 创建一个矩阵数据函数集对象fn_matrix_data。
     assert isinstance(matrix, OpenMaya.MMatrix)  # 断言matrix变量是OpenMaya.MMatrix对象。
-    matrix_obj = fn_matrix_data.create(matrix.inverse())  # 创建一个新的矩阵对象matrix_obj，其中矩阵值为原始矩阵的逆矩阵。
+    matrix_obj = fn_matrix_data.create(
+        matrix.inverse()
+    )  # 创建一个新的矩阵对象matrix_obj，其中矩阵值为原始矩阵的逆矩阵。
     plug_0.setMObject(matrix_obj)  # 将新的矩阵对象设置到属性接口的第二个子接口中。
 
 
@@ -323,7 +363,9 @@ def connect_attr():
     output_plug = mul_fn_dag_node.findPlug("output")  # 找到名为"output"的属性接口output_plug。
 
     color_fn_dag_node = OpenMaya.MFnDependencyNode()  # 创建一个依赖节点函数集对象color_fn_dag_node。
-    color_fn_dag_node.create("blendColors", 'blendColorsA')  # 创建blendColors节点名为blendColorsA
+    color_fn_dag_node.create(
+        "blendColors", "blendColorsA"
+    )  # 创建blendColors节点名为blendColorsA
     input_plug = color_fn_dag_node.findPlug("color1")  # 找到名为"color1"的属性接口input_plug。
 
     modifier = OpenMaya.MDGModifier()  # 创建一个依赖图修改器对象modifier，用于连接属性。
@@ -331,7 +373,7 @@ def connect_attr():
     modifier.doIt()  # 执行连接操作。
 
     color_fn_dag_node1 = OpenMaya.MFnDependencyNode()
-    color_fn_dag_node1.create("blendColors", 'blendColorsB')
+    color_fn_dag_node1.create("blendColors", "blendColorsB")
     input_plug1 = color_fn_dag_node1.findPlug("color1")
 
     modifier1 = OpenMaya.MDGModifier()
@@ -383,7 +425,9 @@ def split_target():
     """创建一个空列表 skin_cluster 用于存储蒙皮集群节点名称"""
     skin_cluster = []
     """使用 executeCommand() 方法执行 MEL 命令来查找与基础物体关联的蒙皮集群节点名称"""
-    OpenMaya.MGlobal.executeCommand("findRelatedSkinCluster " + base_path.fullPathName(), skin_cluster)
+    OpenMaya.MGlobal.executeCommand(
+        "findRelatedSkinCluster " + base_path.fullPathName(), skin_cluster
+    )
     """检查是否找到了蒙皮集群节点，如果没有找到则返回"""
     if len(skin_cluster) == 0:
         return
@@ -437,7 +481,9 @@ def split_target():
     for jnt_id in range(joint_length):
         """使用 MEL 命令复制目标物体，并将复制的物体保存在 duplicate_obj 列表中"""
         duplicate_obj = []
-        OpenMaya.MGlobal.executeCommand("duplicate " + target_path.fullPathName(), duplicate_obj)
+        OpenMaya.MGlobal.executeCommand(
+            "duplicate " + target_path.fullPathName(), duplicate_obj
+        )
         """创建一个 MSelectionList 对象 dup_sel_list，并将复制的物体添加到该列表中"""
         dup_sel_list = OpenMaya.MSelectionList()
         dup_sel_list.add(duplicate_obj[0])
@@ -453,8 +499,9 @@ def split_target():
         """循环遍历目标物体的顶点，并根据偏移值和权重计算分割点的位置，并存储到 split_point 中"""
         for vtx_id in range(target_points.length()):
             split_point.set(
-                base_points[vtx_id] + offset[vtx_id] * weights[vtx_id * joint_path.length() + jnt_id],
-                vtx_id
+                base_points[vtx_id]
+                + offset[vtx_id] * weights[vtx_id * joint_path.length() + jnt_id],
+                vtx_id,
             )
             """使用 setPoints() 方法将分割后的点坐标应用到复制物体的网格上"""
         dup_mesh_fn.setPoints(split_point)
