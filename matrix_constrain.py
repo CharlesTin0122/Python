46a6dfa234a34c7d9b88088c8fa0ede787d2645c
CharlesTin0122 <tianchao0533@gmail.com>
2023-06-13 16:45:38 +0800

    new file:   matrix_constrain.py 	new file:   switched_parent_matrix.py
    Changes not staged for commit:
    	modified:   Learnning/Json_learning.py
    	modified:   Learnning/os_learning.py
    	modified:   Learnning/special_method.py
    	modified:   Learnning/sys_learning.py
    	modified:   matrix_constrain.py
    	modified:   switched_parent_matrix.py
    
    Untracked files:
    	Learnning/maya_api/matrix_constrain.py
    	hik_learnning.py

---
+++ matrix_constrain.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @FileName :  matrix_constrain.py
+# @Author   : TianChao
+# @Email    : tianchao0533@gamil.com
+# @Time     :  2023/6/13 10:26
+# @Software : PyCharm
+# Description: 
+
---
+++ switched_parent_matrix.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @FileName :  switched_parent_matrix.py
+# @Author   : TianChao
+# @Email    : tianchao0533@gamil.com
+# @Time     :  2023/6/13 11:39
+# @Software : PyCharm
+# Description: 
+
