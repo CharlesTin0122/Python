fe15caa1d97591131c12bc118b79d554ed484230
CharlesTin0122 <tianchao0533@gmail.com>
2023-09-19 16:52:36 +0800

    modified:   Examples/jointChildCreater.py 	modified:   Examples/move_skinweights.py 	new file:   fbxImporter/fbx_importer_mgear.py

--- Examples/jointChildCreater.py
+++ Examples/jointChildCreater.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-@FileName    :   jointChildCreater.py
-@DateTime    :   2023/02/27 16:07:43
-@Author  :   Tian Chao 
-@Contact :   tianchao0533@163.com
-"""
-
+'''
+@FileName      : jointChildCreater.py
+@DateTime      : 2023/09/19 16:43:18
+@Author        : Tian Chao
+@Contact       : tianchao0533@163.com
+@Software      : Maya 2023.3
+@PythonVersion : python 3.9.7
+@Description   :
+'''
 import pymel.core as pm
 
 
@@ -21,30 +23,34 @@ def child_jnt_creater(jnt, child_jntname):
         0
     ]  # 复制骨骼为子骨骼并重命名
     pm.parent(child_jnt, jnt)  # 设置子骨骼父子关系
-    skin_cluster = pm.listConnections(jnt, type="skinCluster")[0]  # 获取蒙皮节点
-    pm.skinCluster(
-        skin_cluster, edit=True, addInfluence=str(child_jnt), weight=0
-    )  # 将子骨骼添加到蒙皮节点
-    pm.skinCluster(skin_cluster, edit=True, selectInfluenceVerts=jnt)  # 选择父骨骼蒙皮影响的点
-    pm.skinPercent(
-        skin_cluster, transformMoveWeights=[jnt, child_jnt]
-    )  # 传递父骨骼的蒙皮权重到子骨骼
+    skin_clusters = pm.listConnections(jnt, type="skinCluster")  # 获取蒙皮节点
+    # 列表去重
+    skin_list = list(set(skin_clusters))
+    for skin in skin_list:
+        pm.skinCluster(
+            skin, edit=True, addInfluence=str(child_jnt), weight=0
+        )  # 将子骨骼添加到蒙皮节点
+        pm.skinCluster(skin, edit=True, selectInfluenceVerts=jnt)  # 选择父骨骼蒙皮影响的点
+        pm.skinPercent(
+            skin, transformMoveWeights=[jnt, child_jnt]
+        )  # 传递父骨骼的蒙皮权重到子骨骼
 
 
-child_jnt_creater("pelvis", "pelvis_c")
-child_jnt_creater("spine_01", "spine_01_c")
-child_jnt_creater("spine_02", "spine_02_c")
-child_jnt_creater("spine_03", "spine_03_c")
-child_jnt_creater("neck_01", "neck_01_c")
+if __name__ == "__main__":
+    child_jnt_creater("pelvis", "pelvis_c")
+    child_jnt_creater("spine_01", "spine_01_c")
+    child_jnt_creater("spine_02", "spine_02_c")
+    child_jnt_creater("spine_03", "spine_03_c")
+    child_jnt_creater("neck_01", "neck_01_c")
 
-child_jnt_creater("clavicle_l", "clavicle_child_l")
-child_jnt_creater("upperarm_l", "upperarm_child_l")
-child_jnt_creater("lowerarm_l", "lowerarm_child_l")
-child_jnt_creater("thigh_l", "thigh_child_l")
-child_jnt_creater("calf_l", "calf_child_l")
+    child_jnt_creater("clavicle_l", "clavicle_child_l")
+    child_jnt_creater("upperarm_l", "upperarm_child_l")
+    child_jnt_creater("lowerarm_l", "lowerarm_child_l")
+    child_jnt_creater("thigh_l", "thigh_child_l")
+    child_jnt_creater("calf_l", "calf_child_l")
 
-child_jnt_creater("clavicle_r", "clavicle_child_r")
-child_jnt_creater("upperarm_r", "upperarm_child_r")
-child_jnt_creater("lowerarm_r", "lowerarm_child_r")
-child_jnt_creater("thigh_r", "thigh_child_r")
-child_jnt_creater("calf_r", "calf_child_r")
+    child_jnt_creater("clavicle_r", "clavicle_child_r")
+    child_jnt_creater("upperarm_r", "upperarm_child_r")
+    child_jnt_creater("lowerarm_r", "lowerarm_child_r")
+    child_jnt_creater("thigh_r", "thigh_child_r")
+    child_jnt_creater("calf_r", "calf_child_r")
--- Examples/move_skinweights.py
+++ Examples/move_skinweights.py
@@ -20,27 +20,31 @@ def move_skin_weights(jnt: str, other_jnt: str) -> None:
 
     """
     pm.select(cl=True)  # 取消所有选择
-    skin_cluster = pm.listConnections(jnt, type="skinCluster")[0]  # 获取蒙皮节点
-    pm.skinCluster(skin_cluster, edit=True, selectInfluenceVerts=jnt)  # 选择父骨骼蒙皮影响的点
-    pm.skinPercent(
-        skin_cluster, transformMoveWeights=[jnt, other_jnt]
-    )  # 传递父骨骼的蒙皮权重到子骨骼
+    skin_clusters = pm.listConnections(jnt, type="skinCluster")  # 获取蒙皮节点
+    # 列表去重
+    skin_list = list(set(skin_clusters))
+    for skin_cluster in skin_list:
+        pm.skinCluster(skin_cluster, edit=True, selectInfluenceVerts=jnt)  # 选择父骨骼蒙皮影响的点
+        pm.skinPercent(
+            skin_cluster, transformMoveWeights=[jnt, other_jnt]
+        )  # 传递父骨骼的蒙皮权重到子骨骼
 
 
-move_skin_weights("pelvis", "pelvis_c")
-move_skin_weights("spine_01", "spine_01_c")
-move_skin_weights("spine_02", "spine_02_c")
-move_skin_weights("spine_03", "spine_03_c")
-move_skin_weights("neck_01", "neck_01_c")
+if __name__ == "__main__":
+    move_skin_weights("pelvis", "pelvis_c")
+    move_skin_weights("spine_01", "spine_01_c")
+    move_skin_weights("spine_02", "spine_02_c")
+    move_skin_weights("spine_03", "spine_03_c")
+    move_skin_weights("neck_01", "neck_01_c")
 
-move_skin_weights("clavicle_l", "clavicle_child_l")
-move_skin_weights("upperarm_l", "upperarm_child_l")
-move_skin_weights("lowerarm_l", "lowerarm_child_l")
-move_skin_weights("thigh_l", "thigh_child_l")
-move_skin_weights("calf_l", "calf_child_l")
+    move_skin_weights("clavicle_l", "clavicle_child_l")
+    move_skin_weights("upperarm_l", "upperarm_child_l")
+    move_skin_weights("lowerarm_l", "lowerarm_child_l")
+    move_skin_weights("thigh_l", "thigh_child_l")
+    move_skin_weights("calf_l", "calf_child_l")
 
-move_skin_weights("clavicle_r", "clavicle_child_r")
-move_skin_weights("upperarm_r", "upperarm_child_r")
-move_skin_weights("lowerarm_r", "lowerarm_child_r")
-move_skin_weights("thigh_r", "thigh_child_r")
-move_skin_weights("calf_r", "calf_child_r")
+    move_skin_weights("clavicle_r", "clavicle_child_r")
+    move_skin_weights("upperarm_r", "upperarm_child_r")
+    move_skin_weights("lowerarm_r", "lowerarm_child_r")
+    move_skin_weights("thigh_r", "thigh_child_r")
+    move_skin_weights("calf_r", "calf_child_r")
