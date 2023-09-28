95bcac179fbf5bfc67135323d77f71bb0a280758
CharlesTin0122 <tianchao0533@gmail.com>
2023-09-28 09:35:39 +0800

    modified:   bake_animatian.py

--- bake_animatian.py
+++ bake_animatian.py
@@ -2,7 +2,7 @@
 """
 @FileName    :   bake_animatian.py
 @DateTime    :   2023/06/14 12:09:50
-@Author  :   Tian Chao 
+@Author  :   Tian Chao
 @Contact :   tianchao0533@163.com
 """
 
@@ -17,21 +17,30 @@ import pymel.core as pm
 # TODO: It would be nice to have a little GUI so the animator can choose settings, like range.
 
 
-def bake_selected_joints(bakeBlendshapes=False):
+def bake_selected_joints(everyJoint: list, bakeBlendshapes: bool = False):
+    """为所选骨骼烘焙关键帧
+
+    Args:
+        bakeBlendshapes (bool, optional): 是否烘焙融合变形. Defaults to False.
+    """
+    # 设定起止帧
     startFrame = pm.playbackOptions(minTime=True, q=True)
     endFrame = pm.playbackOptions(maxTime=True, q=True)
-
+    # 设定需要烘焙的动画属性。分别为位移，旋转，缩放
     attrsToBake = ["tx", "ty", "tz", "rx", "ry", "rz", "sx", "sy", "sz"]
-    everyJoint = pm.selected(type="joint")
+    # 设定需要烘焙的骨骼和骨骼属性
+    if not everyJoint:
+        everyJoint = pm.selected(type="joint")
     everyBakeAttr = []
-    for eachNode in everyJoint:
+    # 要需要烘焙的每根骨骼的每个属性放入everyBakeAttr变量
+    for eachJoint in everyJoint:
         for eachAttr in attrsToBake:
-            everyBakeAttr.append(eachNode.attr(eachAttr))
+            everyBakeAttr.append(eachJoint.attr(eachAttr))
 
-        # Then include every custom attribute too:
-        for eachCustomAttr in eachNode.listAttr(userDefined=True):
+        # 也包括自定义属性
+        for eachCustomAttr in eachJoint.listAttr(userDefined=True):
             everyBakeAttr.append(eachCustomAttr)
-
+    # 如果烘焙融合变形
     if bakeBlendshapes:
         # Include baking all blendshapes
         targetBlendshapes = pm.selected(type="blendShape")
@@ -40,7 +49,7 @@ def bake_selected_joints(bakeBlendshapes=False):
                 if eachTarget.isLocked():
                     continue
                 everyBakeAttr.append(eachTarget)
-
+    # 烘焙关键帧
     pm.bakeResults(
         everyBakeAttr,
         simulation=True,
@@ -53,9 +62,11 @@ def bake_selected_joints(bakeBlendshapes=False):
         t=(startFrame, endFrame),
     )
 
-    # Run an euler filter to help prevent flipping.
+    # 执行欧拉过滤器以防止动画曲线翻转
     everyRotation = [jnt.r for jnt in everyJoint]
     pm.filterCurve(everyRotation, filter="euler")
 
 
-bake_selected_joints(bakeBlendshapes=True)
+if __name__ == "__main__":
+    jnt_list = pm.selected(type="joint")
+    bake_selected_joints(jnt_list)
