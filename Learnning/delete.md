d8d38a18bf47d019c9dc2fb51df76e19ff88b45b
CharlesTin0122 <tianchao0533@gmail.com>
2024-04-10 17:33:55 +0800

    deleted:    Learnning/Hello.py 	deleted:    Learnning/Json_learning.py 	deleted:    Learnning/class01.py 	deleted:    Learnning/class02.py 	deleted:    Learnning/combine.py 	deleted:    Learnning/decorator.py 	deleted:    Learnning/delete.md 	deleted:    Learnning/dictionary_learn.py 	deleted:    Learnning/for_Loop.py 	deleted:    Learnning/function.py 	deleted:    Learnning/function2.py 	deleted:    Learnning/generator_learn.py 	deleted:    Learnning/if.py 	deleted:    Learnning/iterator_learn.py 	deleted:    Learnning/lambda_learning.py 	deleted:    Learnning/list.py 	deleted:    Learnning/logging_learn.py 	deleted:    Learnning/os_learning.py 	deleted:    Learnning/pSphere1.json 	deleted:    Learnning/path.json 	deleted:    Learnning/pymel.py 	deleted:    Learnning/string_format.py 	deleted:    Learnning/sys_learning.py 	deleted:    Learnning/tryExcept.py 	deleted:    Learnning/ui.py 	deleted:    Learnning/while_loop.py

--- Learnning/delete.md
+++
@@ -1,4 +0,0 @@
-# asd
-## afdsg 
-### dsfhgnf
-awasgdgfdfdjnklml
--- Learnning/pymel.py
+++
@@ -1,9 +0,0 @@
-import pymel.core as pm
-
-
-a = pm.selected()
-t = a[0].getTranslation()
-r = a[0].getRotation()
-s = a[0].getScale()
-att = a[0].listAnimatable()
-dir(a[0])
